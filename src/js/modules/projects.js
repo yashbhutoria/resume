869334a9dc052b724757b99812c35fa5a8609441
Mike Joyce <contact@mikejoyce.me>
2018-08-08 00:18:10 +0700

    Update projects

--- src/js/modules/projects.js
+++ src/js/modules/projects.js
@@ -17,49 +17,65 @@ const projects = {
   'header': 'Projects',
   'projects': [
     {
+      'title': 'Foundation 6 + React Boilerplate',
+      'url': 'https://github.com/mikejoyceio/foundation-react-boilerplate',
+      'dates': 'Aug 2018',
+      'description': 'A minimal Foundation for Sites 6 and React boilerplate with Webpack to streamline common front-end tasks.'
+    },
+    {
+      'title': 'ADA Compliance Resource',
+      'url': 'https://github.com/mikejoyceio/ada-compliance',
+      'dates': 'Aug 2018',
+      'description': 'A resource to document Americans with Disabilities Act (ADA) Compliance requirements.'
+    },
+    {
+      'title': '100% Clean for PNW',
+      'url': 'http://100forpnw.org/',
+      'dates': 'Dec 2017',
+      'description': 'Campaign website for 100% Clean for PNW. Sketch translation to HTML/CSS/JavaScript.'
+    },
+    {
+      'title': 'World Sight Day',
+      'url': 'http://wsd.seenow.org/',
+      'dates': 'Nov 2017',
+      'description': 'World Sight Day campaign website for <a class="link-text" href="http://seenow.org/" target="_blank">SeeNow</a>. Sketch translation to HTML/CSS/JavaScript.'
+    },
+    {
+      'title': 'Meet Hope',
+      'url': 'https://www.meethope.us/',
+      'dates': 'Sep 2017',
+      'description': 'Meet Hope website for <a class="link-text" href="http://www.purpose.com/" target="_blank">Purpose</a>, which won an Awwwards Honorable Mention in 2017. Sketch translation to HTML/CSS/JavaScript.'
+    },
+    {
+      'title': 'JavaScript Testing',
+      'url': 'https://github.com/mikejoyceio/javascript-testing',
+      'dates': 'May 2015',
+      'description': 'A Jasmine test suite for a web-based application that reads RSS feeds.'
+    },
+    {
       'title': 'Map Panda',
-      'url': 'http://mappanda.co',
-      'dates': '2015',
-      'description': 'Map Panda is a single-page JavaScript application that grabs the user\'s location with the Geolocation API, and through the Google Places API a selection of popular location types are available to explore. The Foursquare and Uber APIs are leveraged to provide further functionality to the application.',
-      'images': [
-        'dist/images/mapPanda01.jpg',
-        'dist/images/mapPanda02.jpg',
-        'dist/images/mapPanda03.jpg'
-      ]
+      'url': 'https://github.com/mikejoyceio/map',
+      'dates': 'May 2015',
+      'description': 'Map Panda is a single-page application that grabs the user\'s location with the Geolocation API, and through the Google Places API a selection of popular location types are available to explore on a Google map. The Foursquare and Uber APIs are leveraged to provide further functionality to the application.'
+    },
+    {
+      'title': 'Website Performance Optimization',
+      'url': 'https://github.com/mikejoyceio/website-optimization',
+      'dates': 'Feb 2015',
+      'description': 'A step-by-step rundown of optimizations made to a website with a number of optimization and performance-related issues so that it achieves a high PageSpeed Insights score and runs at 60 frames per second.'
+    },
+    {
+      'title': 'Retro Frogger',
+      'url': 'https://github.com/mikejoyceio/arcade-game',
+      'dates': 'Jan 2015',
+      'description': 'A step-by-step rundown of optimizations made to a website with a number of optimization and performance-related issues so that it achieves a high PageSpeed Insights score and runs at 60 frames per second.'
     },
     {
       'title': 'iOpenDecision',
-      'url': 'http://citygategis.com/products/iopengov',
+      'url': 'https://citygategis.com/products/iopengov',
       'dates': '2014 - Present',
-      'description': 'iOpenDecision is a web based application which enables the development of cost-benefit models which include a geographic component.  A collaborative approach is used to allow subject matter experts in different areas to come together and create a financial model.  This approach allows for a more comprehensive understanding of the nature of the costs and the valued benefits of a project. <br><br> The software is currently being used by The Maryland Department of Transportation (MDOT), a government agency in the U.S. state of Maryland',
-      'images': [
-        'dist/images/iOpenDecision01.jpg',
-        'dist/images/iOpenDecision02.jpg',
-        'dist/images/iOpenDecision03.jpg'
-      ]
+      'description': 'iOpenDecision is a web based application which enables the development of cost-benefit models which include a geographic component.  A collaborative approach is used to allow subject matter experts in different areas to come together and create a financial model.  This approach allows for a more comprehensive understanding of the nature of the costs and the valued benefits of a project. <br><br> The software is currently being used by The Maryland Department of Transportation (MDOT), a government agency in the U.S. state of Maryland'
     },
-    {
-      'title': 'Here Now',
-      'url': 'http://weareherenow.com',
-      'dates': '2014',
-      'description': 'Here Now landing page for <a class="link-text" href="http://purpose.com" target="_blank">Purpose</a>. Commissioned by New York based agency, <a class="link-text" href="http://3-tone.com" target="_blank">3tone</a>. PSD to HTML. Built with Foundation 5, jQuery and Greensock.',
-      'images': [
-        'dist/images/hereNow01.jpg',
-        'dist/images/hereNow02.jpg',
-        'dist/images/hereNow03.jpg'
-      ]
-    },
-    {
-      'title': 'Date Asia',
-      'url': 'https://date-asia.com/?skin=def_mobile',
-      'dates': '2014 - Present',
-      'description': 'An in-progress responsive reboot of a personal project that I started in an attempt to improve my front-end development skills. I learn well by doing, and this project gives me a chance to put new skills I\'ve gained into practice. An open source PHP dating script on the backend, Bootstrap 3 on the front end.',
-      'images': [
-        'dist/images/dateAsia01.jpg',
-        'dist/images/dateAsia02.jpg',
-        'dist/images/dateAsia03.jpg'
-      ]
-    }
   ]
 };
 
@@ -88,7 +104,7 @@ for (var project in projects.projects) {
 
   $('.project-entry:last').append(HTMLprojectImageStart);
 
-  if (projects.projects[project].images.length > 0) {
+  if (projects.projects[project].images) {
     for (var image in projects.projects[project].images) {
       let formattedImage = HTMLprojectImage.replace('%data%', projects.projects[project].images[image])
       $('.project-images:last').append(formattedImage);
