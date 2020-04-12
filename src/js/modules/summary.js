f5411b633e89ee8d35b63fef0e28b27f32482774
Mike Joyce <contact@mikejoyce.me>
2020-04-13 06:12:26 +0700

    Update summary

--- src/js/modules/summary.js
+++ src/js/modules/summary.js
@@ -16,9 +16,9 @@ const $summary = $('#summary');
 const summary = {
   'header': 'Summary',
   'paragraphs': [
-    'I am a highly dedicated Front-End Developer, and after having worked remotely for a number of clients for 4+ years now, I genuinely love the work that I do! I strive to give 100%, and my goal as a developer is to write highly organised, readable code which ultimately produces an excellent and accessible user experience.',
+    'I am a highly dedicated Front-End Developer, and after having worked remotely for a number of clients for 5+ years now, I genuinely love the work that I do! I strive to give 100%, and my goal as a developer is to write highly organised, readable code which ultimately produces an excellent and accessible user experience.',
     'Collaborating with and learning from others, self-guided learning and doing my best to keep up to date with current and emerging technologies helps me to thrive and continuously improve as a developer in an industry that is constantly evolving. I’m proactive when it comes to communication, adaptable, and can organize and prioritize tasks under pressure.',
-    'Currently, I’m focusing on both React and React Native, which I hope to eventually specialize in.'
+    'Currently, I’m focusing on both React and Vue, which I hope to eventually specialize in.'
   ]
 };
 
